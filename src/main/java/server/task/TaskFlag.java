3c0849094a60625d821af5b2ec4b8821b681a732
mmtos <js223kr92@naver.com>
2022-01-31 01:50:05 +0900

    TaskFlag 정의

---
+++ src/main/java/server/task/TaskFlag.java
@@ -0,0 +1,25 @@
+package server.task;
+
+import java.util.EnumSet;
+
+public enum TaskFlag {
+    ECHO_TASK("A"),NULL_TASK("");
+
+    String flag;
+
+    TaskFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public static TaskFlag getTaskFlag(String flag) throws UnsupportedOperationException {
+        EnumSet<TaskFlag> tasks = EnumSet.allOf(TaskFlag.class);
+        for (TaskFlag task : tasks) {
+            if (task.flag.equals(flag)) return task;
+        }
+        throw new UnsupportedOperationException();
+    }
+}
