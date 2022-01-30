66514d2a089af5dc8f42b667250b2da6ae7aab37
mmtos <js223kr92@naver.com>
2022-01-30 18:20:59 +0900

    간단한 메시지를 전달하는 Client 코드 작성

---
+++ src/main/java/ClientMain.java
@@ -0,0 +1,32 @@
+import config.GlobalConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientMain {
+    static Logger log = LoggerFactory.getLogger(ClientMain.class);
+    public static void main(String[] args) {
+        final String localhost = "127.0.0.1";
+        try(Socket clientSocket = new Socket(localhost, GlobalConfig.getConfig().getServerSocketPort())){
+            //https://stackoverflow.com/questions/47175526/try-with-multiple-resource-in-java
+            try(PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ){
+                out.println("12345");
+                String line = "";
+                while((line = in.readLine()) != null ){
+                    log.debug(line);
+                }
+            }
+        }catch (IOException e){
+            log.error("연결실패");
+        }
+
+
+    }
+}
